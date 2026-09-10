18adce083fd47034c550416b1c7b4b2b62915b6a
Max Morris <142640020+maxmorris1@users.noreply.github.com>
2026-09-10 12:08:56 +0800

    Auto-commit: Thu 10/09/2026 12:08:56.69

---
+++ QueryDB.py
@@ -0,0 +1,70 @@
+import sqlite3
+import hashlib
+
+db_file = "database.db"
+sql_file = "SQL Schema.sql"
+
+conn = sqlite3.connect(db_file)
+cursor = conn.cursor()
+with open(sql_file, 'r') as sql_file:
+    sql_script = sql_file.read()
+
+cursor.executescript(sql_script)
+
+conn.commit()
+conn.close()
+
+def db_action(action, info_array):
+    conn = sqlite3.connect(db_file)
+    cursor = conn.cursor()
+    if action == 'check staff credentials':
+        username, password = info_array
+        cursor.execute("SELECT 1, StaffPassHex FROM StaffLogin WHERE StaffUsername = ?", (username,))
+        row = cursor.fetchone()
+        if row is None:
+            return "New"
+        _, password_check = row
+        passHash = hashlib.sha256(password.encode('utf-8'))
+        passHashDig = passHash.hexdigest()
+        if passHashDig == password_check:
+            credientials_check = 'Correct'
+        else:
+            credientials_check = 'Incorrect'
+        return credientials_check
+    elif action == 'new staff credentials':
+        new_credientials, firstName, lastName, staffPosition = info_array()
+        username, password = new_credientials()
+        passHash = hashlib.sha256(password.encode('utf-8'))
+        passHashDig = passHash.hexdigest()
+        new_staff_member = (firstName, lastName, staffPosition) 
+        cursor.execute("INSERT INTO Staff (StaffFirstName, StaffLastName, StaffPosition) VALUES (?, ?, ?)", new_staff_member)
+        staff_id = cursor.lastrowid
+        new_staff_creds = (staff_id, username, passHashDig)
+        cursor.execute("INSERT INTO StaffLogin (StaffID, StaffUsername, StaffPassHex) VALUES (?, ?, ?)", new_staff_creds)
+    elif action == 'check customer credentials':
+        username, password = info_array
+        cursor.execute("SELECT 1, CustomerPassHex FROM StaffLogin WHERE CustomerUsername = ?", (username,))
+        row = cursor.fetchone()
+        if row is None:
+            return "New"
+        _, password_check = row
+        passHash = hashlib.sha256(password.encode('utf-8'))
+        passHashDig = passHash.hexdigest()
+        if passHashDig == password_check:
+            credientials_check = 'Correct'
+        else:
+            credientials_check = 'Incorrect'
+        return credientials_check
+    elif action == 'new customer credentials':
+        new_credientials, firstName, lastName, phone, email = info_array()
+        username, password = new_credientials()
+        passHash = hashlib.sha256(password.encode('utf-8'))
+        passHashDig = passHash.hexdigest()
+        new_customer = (firstName, lastName, phone, email) 
+        cursor.execute("INSERT INTO Customer (CustomerFirstName, CustomerLastName, CustomerPhone, CustomerEmail) VALUES (?, ?, ?)", new_customer)
+        customer_id = cursor.lastrowid
+        new_customer_creds = (customer_id, username, passHashDig)
+        cursor.execute("INSERT INTO CustomerLogin (CustomerID, CustomerUsername, CustomerPassHex) VALUES (?, ?, ?)", new_customer_creds)
+    conn.close()
+
+
--- interface.py
+++ interface.py
@@ -4,7 +4,13 @@ import os
 import msvcrt
 import shutil
 import subprocess
+from time import sleep
 from pathlib import Path
+from QueryDB import db_action
+
+RED = "\033[31m"
+BOLD_GREEN = "\033[1;32m"
+RESET = "\033[0m"
 
 def install_requirements():
     requirements = Path(__file__).with_name("requirements.txt")
@@ -73,7 +79,7 @@ def padding(text):
         sys.stdout.write(" " * padding)
         sys.stdout.flush()
 
-def type_out(text, delay=0.01):
+def type_out(text, delay=0.002):
     padding(text)
     for char in text:
       sys.stdout.write(char)
@@ -83,22 +89,37 @@ def type_out(text, delay=0.01):
 
 def staff_or_customer():
     clear_terminal()
-    padding_top(8)
+    padding_top(19)
     sys.stdout.write("\033[?25h")
-    type_out(' '+'_'*48+' ')
-    type_out('|'+' '*48+'|')
-    type_out('|'+' '*48+'|')
-    type_out('|'+' '*21+'Login'+' '*22+'|')
-    type_out('|'+' '*48+'|')
-    type_out('|'+' '*15+'> Staff'+' '*26+'|')
-    type_out('|'+' '*17+'Customer'+' '*23+'|')
-    type_out('|'+' '*48+'|')
-    type_out('|'+' '*48+'|')
-    type_out('|'+'_'*48+'|')
+    type_out('╔'+'═'*48+'╗')
+    type_out('║'+' '*48+'║')
+    type_out('║'+' '*48+'║')
+    type_out('║'+' '*48+'║')
+    type_out('║'+' '*48+'║')
+    type_out('║'+' '*48+'║')
+    type_out('║'+' '*48+'║')
+    type_out('║'+' '*21+'Login'+' '*22+'║')
+    type_out('║'+' '*48+'║')
+    type_out('║'+' '*48+'║')
+    type_out('║'+' '*17+'Staff'+' '*26+'║')
+    type_out('║'+' '*17+'Customer'+' '*23+'║')
+    type_out('║'+' '*48+'║')
+    type_out('║'+' '*48+'║')
+    type_out('║'+' '*48+'║')
+    type_out('║'+' '*48+'║')
+    type_out('║'+' '*48+'║')
+    type_out('║'+' '*48+'║')
+    type_out('╚'+'═'*48+'╝')
     role = 'Staff'
     sys.stdout.write("\033[?25l")
     sys.stdout.flush()
-    sys.stdout.write("\033[5A\r")
+    sys.stdout.write("\033[9A\r")
+    sys.stdout.flush()
+    padding(' '*50)
+    print('║'+' '*15+f'{BOLD_GREEN}> Staff{RESET}'+' '*26+'║')
+    padding(' '*50)
+    print('║'+' '*17+'Customer'+' '*23+'║')
+    sys.stdout.write("\033[2A\r")
     sys.stdout.flush()
     def on_press(key):
         nonlocal role
@@ -106,18 +127,18 @@ def staff_or_customer():
             if role == 'Customer':
                 role = 'Staff'
                 padding(' '*50)
-                print('|'+' '*15+'> Staff'+' '*26+'|')
+                print('║'+' '*15+f'{BOLD_GREEN}> Staff{RESET}'+' '*26+'║')
                 padding(' '*50)
-                print('|'+' '*17+'Customer'+' '*23+'|')
+                print('║'+' '*17+'Customer'+' '*23+'║')
                 sys.stdout.write("\033[2A\r")
                 sys.stdout.flush()
         elif key == keyboard.Key.down or (hasattr(key, 'char') and key.char == 's'):
             if role == 'Staff':
                 role = 'Customer'
                 padding(' '*50)
-                print('|'+' '*17+'Staff'+' '*26+'|')
+                print('║'+' '*17+'Staff'+' '*26+'║')
                 padding(' '*50)
-                print('|'+' '*15+'> Customer'+' '*23+'|')
+                print('║'+' '*15+f'{BOLD_GREEN}> Customer{RESET}'+' '*23+'║')
                 sys.stdout.write("\033[2A\r")
                 sys.stdout.flush()
         elif key == keyboard.Key.enter:
@@ -134,20 +155,28 @@ def staff_or_customer():
 
 def login(role):
     clear_terminal()
-    padding_top(10)
+    padding_top(19)
     sys.stdout.write("\033[?25h")
     sys.stdout.flush()
-    padding(' '*50); print(' '+'_'*48+' ')
-    padding(' '*50); print('|'+' '*48+'|')
-    padding(' '*50); print('|'+' '*48+'|')
-    padding(' '*50); print('|'+' '*48+'|')
-    padding(' '*50); print('|'+' '*48+'|')
-    padding(' '*50); print('|'+' '*48+'|')
-    padding(' '*50); print('|'+' '*48+'|')
-    padding(' '*50); print('|'+' '*48+'|')
-    padding(' '*50); print('|'+' '*48+'|')
-    padding(' '*50); print('|'+' '*48+'|')
-    padding(' '*50); print('|'+'_'*48+'|')
+    padding(' '*50); print('╔'+'═'*48+'╗')
+    padding(' '*50); print('║'+' '*48+'║')
+    padding(' '*50); print('║'+' '*48+'║')
+    padding(' '*50); print('║'+' '*48+'║')
+    padding(' '*50); print('║'+' '*48+'║')
+    padding(' '*50); print('║'+' '*48+'║')
+    padding(' '*50); print('║'+' '*48+'║')
+    padding(' '*50); print('║'+' '*48+'║')
+    padding(' '*50); print('║'+' '*48+'║')
+    padding(' '*50); print('║'+' '*48+'║')
+    padding(' '*50); print('║'+' '*48+'║')
+    padding(' '*50); print('║'+' '*48+'║')
+    padding(' '*50); print('║'+' '*48+'║')
+    padding(' '*50); print('║'+' '*48+'║')
+    padding(' '*50); print('║'+' '*48+'║')
+    padding(' '*50); print('║'+' '*48+'║')
+    padding(' '*50); print('║'+' '*48+'║')
+    padding(' '*50); print('║'+' '*48+'║')
+    padding(' '*50); print('╚'+'═'*48+'╝')
     columns, _ = shutil.get_terminal_size()
     left_margin = (columns - 50) // 2
     if role == 'Staff':
@@ -156,7 +185,7 @@ def login(role):
     else:
         role_margin = 17
         role_input_limit = 19
-    sys.stdout.write(f"\033[7A\r\033[{left_margin + 1 + role_margin}C")
+    sys.stdout.write(f"\033[12A\r\033[{left_margin + 1 + role_margin}C")
     sys.stdout.flush()
     for char in f"{role} Login":
         sys.stdout.write(char)
@@ -164,7 +193,7 @@ def login(role):
         time.sleep(0.02)
     sys.stdout.write("\033[?25l")
     sys.stdout.flush()
-    sys.stdout.write(f"\033[2B\r\033[{left_margin + 1 + 10}C")
+    sys.stdout.write(f"\033[3B\r\033[{left_margin + 1 + 10}C")
     sys.stdout.flush()
     sys.stdout.write("\033[?25h")
     sys.stdout.flush()
@@ -183,35 +212,60 @@ def login(role):
     sys.stdout.flush()
 
     padding(' '*50)
-    username = limit_input('|'+' '*10+f'{role} Username:', role_input_limit)
+    username = limit_input('║'+' '*10+f'{role} Username:', role_input_limit)
     if username == 'BACK':
         while msvcrt.kbhit():
             msvcrt.getch()
         return 'BACK', 'BACK'
     while len(username) < 4:
+        if len(username) > 0:
+            sys.stdout.write("\033[1A\r")
+            sys.stdout.flush()
+            padding(' '*50)
+            print('║'+' '*10+ RED + f'{role} Username:' + username + RESET)
+            sys.stdout.write("\033[?25l")
+            sys.stdout.flush()
+            time.sleep(0.5)
+            sys.stdout.write("\033[?25h")
+            sys.stdout.flush()
         sys.stdout.write("\033[1A\r")
         sys.stdout.flush()  
         padding(' '*50)
-        print('|'+' '*10+f'{role} Username:'+' '*(role_input_limit+1)+'|')
+        print('║'+' '*10+f'{role} Username:'+' '*(role_input_limit+1)+'║')
         sys.stdout.write("\033[1A\r")
         sys.stdout.flush()  
         padding(' '*50)
-        username = limit_input('|'+' '*10+f'{role} Username:', role_input_limit)
+        username = limit_input('║'+' '*10+f'{role} Username:', role_input_limit)
 
-
+    if username == 'BACK':
+        while msvcrt.kbhit():
+            msvcrt.getch()
+        return 'BACK', 'BACK'
     padding(' '*50)
-    password = limit_input('|'+' '*10+f'{role} Password:', role_input_limit)
+    password = limit_input('║'+' '*10+f'{role} Password:', role_input_limit)
     if password == 'BACK':
-        return 'BACK', password
+        while msvcrt.kbhit():
+            msvcrt.getch()
+        return 'BACK', 'BACK'
     while len(password) < 4:
+        if len(password) > 0:
+            sys.stdout.write("\033[1A\r")
+            sys.stdout.flush()
+            padding(' '*50)
+            print('║'+' '*10+ RED + f'{role} Password:' + password + RESET)
+            sys.stdout.write("\033[?25l")
+            sys.stdout.flush()
+            time.sleep(0.5)
+            sys.stdout.write("\033[?25h")
+            sys.stdout.flush()
         sys.stdout.write("\033[1A\r")
         sys.stdout.flush()  
         padding(' '*50)
-        print('|'+' '*10+f'{role} Password:'+' '*(role_input_limit+1)+'|')
+        print('║'+' '*10+f'{role} Password:'+' '*(role_input_limit+1)+'║')
         sys.stdout.write("\033[1A\r")
         sys.stdout.flush()  
         padding(' '*50)
-        password = limit_input('|'+' '*10+f'{role} Password:', role_input_limit)
+        password = limit_input('║'+' '*10+f'{role} Password:', role_input_limit)
     
     sys.stdout.write("\033[H")
     sys.stdout.flush()
@@ -219,6 +273,108 @@ def login(role):
 
     return username, password
 
+def staff_signup():
+    clear_terminal()
+    padding_top(19)
+    sys.stdout.write("\033[?25h")
+    sys.stdout.flush()
+    padding(' '*50); print('╔'+'═'*48+'╗')
+    padding(' '*50); print('║'+' '*48+'║')
+    padding(' '*50); print('║'+' '*48+'║')
+    padding(' '*50); print('║'+' '*48+'║')
+    padding(' '*50); print('║'+' '*48+'║')
+    padding(' '*50); print('║'+' '*48+'║')
+    padding(' '*50); print('║'+' '*48+'║')
+    padding(' '*50); print('║'+' '*48+'║')
+    padding(' '*50); print('║'+' '*48+'║')
+    padding(' '*50); print('║'+' '*48+'║')
+    padding(' '*50); print('║'+' '*48+'║')
+    padding(' '*50); print('║'+' '*48+'║')
+    padding(' '*50); print('║'+' '*48+'║')
+    padding(' '*50); print('║'+' '*48+'║')
+    padding(' '*50); print('║'+' '*48+'║')
+    padding(' '*50); print('║'+' '*48+'║')
+    padding(' '*50); print('║'+' '*48+'║')
+    padding(' '*50); print('║'+' '*48+'║')
+    padding(' '*50); print('╚'+'═'*48+'╝')
+    columns, _ = shutil.get_terminal_size()
+    left_margin = (columns - 50) // 2
+    title = "New Staff Member"
+    question_1 = "First Name:"
+    question_2 = "Last Name:"
+    question_3 = "Staff Position:"
+    inner_title_margin = (48 - len(title)) // 2
+    sys.stdout.write(f"\033[13A\r\033[{left_margin + 1 + inner_title_margin}C")
+    sys.stdout.flush()
+    for char in title:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(0.02)
+    sys.stdout.write(f"\033[3B\r\033[{left_margin + 1 + 10}C")
+    sys.stdout.flush()
+    for char in question_1:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(0.02)
+    sys.stdout.write(f"\033[1B\r\033[{left_margin + 1 + 10}C")
+    sys.stdout.flush()
+    for char in question_2:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(0.02)
+    sys.stdout.write(f"\033[1B\r\033[{left_margin + 1 + 10}C")
+    sys.stdout.flush()
+    for char in question_3:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(0.02)
+
+    def question_input_limit(question_text):
+        return 48 - 10 - len(question_text)
+
+    def print_question(question_text):
+        padding(' '*50)
+        input_text = limit_input('║'+' '*10+question_text, question_input_limit(question_text))
+        if input_text == 'BACK':
+            return 'BACK'
+        while len(input_text) == 0:
+            sys.stdout.write("\033[1A\r")
+            sys.stdout.flush()
+            padding(' '*50)
+            print('║'+' '*10+ RED + question_text + RESET)
+            sys.stdout.write("\033[?25l")
+            sys.stdout.flush()
+            time.sleep(0.5)
+            sys.stdout.write("\033[?25h")
+            sys.stdout.flush()
+            sys.stdout.write("\033[1A\r")
+            sys.stdout.flush()  
+            padding(' '*50)
+            print('║'+' '*10+question_text+' '*question_input_limit(question_text)+'║')
+            sys.stdout.write("\033[1A\r")
+            sys.stdout.flush()  
+            padding(' '*50)
+            input_text = limit_input('║'+' '*10+question_text, question_input_limit(question_text))
+        return input_text
+
+    sys.stdout.write("\033[2A\r")
+    sys.stdout.flush()
+    staff_Name = print_question(question_1)
+    if staff_Name == 'BACK':
+        while msvcrt.kbhit():
+            msvcrt.getch()
+        return 'BACK', 'BACK', 'BACK'
+    staff_Lastname = print_question(question_2)
+    if staff_Lastname == 'BACK':
+        while msvcrt.kbhit():
+            msvcrt.getch()
+        return 'BACK', 'BACK', 'BACK'
+    staff_Position = print_question(question_3)
+    if staff_Position == 'BACK':
+        while msvcrt.kbhit():
+            msvcrt.getch()
+        return 'BACK', 'BACK', 'BACK'
+    return staff_Name, staff_Lastname, staff_Position
 
 def main():
     while True:
@@ -226,7 +382,13 @@ def main():
         username, password = login(role)
         if username == 'BACK' or password == 'BACK':
             continue
+        if role == 'Staff':
+            check_credentials = db_action('check staff credentials', [username, password])
+            if check_credentials == 'New':
+                staffName, staffLastname, staffPosition = staff_signup()
+                if staffName == 'BACK' or staffLastname == 'BACK' or staffPosition == 'BACK':
+                    continue
+                info_array = [[username, password], staffName, staffLastname, staffPosition]
+                db_action('new staff credentials', info_array)
         break
-    print(f'Role: {role}, Username: {username}, Password: {password}')
-
 main()
